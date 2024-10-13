9dc87ffa67c1b7d2ae13e4e20cfaa5658b8c7e97
Deekrajput <128222092+Deekrajput@users.noreply.github.com>
2024-10-13 19:34:25 +0530

    Update db.js

--- login backend/db.js
+++ login backend/db.js
@@ -1,14 +1,17 @@
-// db.js
 const mongoose = require('mongoose');
-const URL="mongodb://localhost:27017/Login"
+
+// Replace with your MongoDB Atlas connection string
+const URL = "mongodb+srv://deekrajput94:<WVXm9fGXjhJLi6Ww>@cluster0.zu9qg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const connectToMongo = async () => {
     try {
         await mongoose.connect(URL, {
-            
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
-        console.log('Connected to MongoDB successfully');
+        console.log('Connected to MongoDB Atlas successfully');
     } catch (error) {
-        console.error('Failed to connect to MongoDB', error);
+        console.error('Failed to connect to MongoDB Atlas', error);
         process.exit(1);
     }
 };
